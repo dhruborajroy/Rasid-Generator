1024aa4591ee9c97b8379c24f70cd739496ef7e0
Dhrubo <dhruborajroy3@gmail.com>
2024-08-30 15:12:30 +0600

    updated bijoy

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 
 <?php
-
+include_once __DIR__.'/U2B.php';
+use Eyamin\U2B as U2B;
 require('fpdf.php');
 $pdf=new FPDF();
 $pdf->SetAuthor("Dhrubo Raj Roy-http://TheDhrubo.xyz");
 
     $font="fonts/Mainak-Buniyadi-V1.ttf";
+    // $font="fonts/v1.ttf";
     $php_array = json_decode(file_get_contents('names.json'), true);
     $count=count($php_array);
     for ($i=0; $i < $count; $i++) {
-        $image=imagecreatefromjpeg("pics/rasid-2023.jpg");
+        $image=imagecreatefromjpeg("pics/rasid-2024.jpg");
         $color=imagecolorallocate($image,0,0,0);
-        imagettftext($image,55,0,580,645,$color,$font,$php_array[$i]['name_bijoy']);
+        imagettftext($image,55,0,580,660,$color,$font,U2B::convert($php_array[$i]['name_avro']));
         if($php_array[$i]['donation_2022']>1 && $php_array[$i]['donation_2022']<499){
             $amount=500;
         }elseif($php_array[$i]['donation_2022']>500 && $php_array[$i]['donation_2022']<1000){
@@ -29,17 +31,17 @@ $pdf->SetAuthor("Dhrubo Raj Roy-http://TheDhrubo.xyz");
         }elseif($php_array[$i]['donation_2022']>=3000){
             $amount=5000;
         }else{
-            // $amount= $php_array[$i]['donation_2022'];
+            $amount= $php_array[$i]['donation_2022'];
             $amount=1000;
         }
-        imagettftext($image,55,0,440,745,$color,$font,$amount." UvKv");
-        imagettftext($image,55,0,1240,745,$color,$font,'Av`k© cvov');
+        // imagettftext($image,55,0,440,745,$color,$font,$amount." UvKv");
+        imagettftext($image,55,0,1240,765,$color,$font,'Av`k© cvov');
         
         $file=" ".$i." ".'_'.$i;
-        $pdf->SetTitle($php_array[$i]['goli_name']);
+        $pdf->SetTitle("Rasid ".Date("Y"));
                 
         $file_path=$i.".jpg";
-        $file_path_pdf=$php_array[$i]['goli_name'].".pdf";
+        $file_path_pdf="Rasid ".Date("Y").".pdf";
         imagejpeg($image,$file_path);
         imagedestroy($image);
 
